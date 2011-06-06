09550723ef552318ebdb346a3658c3d74c1f300b
Paul Brannan <curlypaul924@gmail.com>
2011-06-06 15:40:23 -0400

    Moved files to etc dir.

--- etc/config.rb
+++ etc/config.rb
@@ -12,7 +12,7 @@ class CONFIG
     CHANNELS        = [ '#alt-255' ]
     # BOTLIST         = [ 'Alt-255', 'cout' ]
     BOTLIST         = []
-    PRIVATE_KEY     = File.readlines('private_key.pem')
+    PRIVATE_KEY     = File.readlines(File.join(__FILE__, 'private_key.pem'))
     TIMESTAMP       = true
     RECONNECT_DELAY = 300
     PING_INTERVAL   = 300
