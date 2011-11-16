50aead1a79d0f7cc0a9f17575c1b942f1c14ef4d
alt-255 <alt-255@rumble.withoutpenandink.com>
2011-11-15 22:32:09 -0500

    Fixed units command.

--- lib/alt-255/commands/units.rb
+++ lib/alt-255/commands/units.rb
@@ -7,14 +7,27 @@ class UnitsCommand < Command
   LOGGABLE = true
 
   def do(command)
+    case command.str
+    when  /(.*?)\s+to\s+(.*)/i
+      units_from = $1
+      units_to = $2
+    else
+      a = command.str.split
+      units_from = a[0..-2].join(' ')
+      units_to = a[-1]
+    end
+
     # Execute /usr/bin/units with the specified command
-    # units_from.untaint; units_to.untaint
-    # input, output, error = Open3.popen3('/usr/bin/units -q --verbose')
-    # input.puts units_from
-    # input.puts units_to
-    # input.close
-    # output.gets; output.gets # eat input
-    # result = "Units says: #{output.gets.strip}" # hmm, this does not work
-    # privmsg(dest, result)
+    units_from.untaint; units_to.untaint
+    input, output, error = Open3.popen3('/usr/bin/units -q --verbose')
+    begin
+      input.puts units_from
+      input.puts units_to
+      output.gets; output.gets # eat input
+      command.reply("Units says: #{output.gets.strip}")
+    ensure
+      input.close
+      output.close
+    end
   end
 end
